c4e8b14957b0d891d0a582b129dc21ea3fc706b8
r0664n77 <4dmiraldoberman@gmail.com>
2020-11-29 19:15:26 +0300

    Tg2

---
+++ README.md
@@ -0,0 +1,39 @@
+# TriangleGram2
+![Клиент Telegram на основе библиотеки telethon с гибридным шифрованием. Полностью написан на Python](https://notabug.org/Black_Triangle/TriangleGram2/raw/master/prewy.png)
+
+Клиент Telegram на основе библиотеки telethon с шифрованием и стеганографией. Полностью написан на Python
+
+### Зависимости:
+
+`pip3 install -r requirements.txt`
+
+`sudo apt-get install python3-tk`
+
+`sudo apt install npm`
+
+`sudo npm install -g npm`
+
+`sudo npm install -g stegcloak`
+
+Запустить фаил `1.py`и следовать инструкциям программы.
+
+## Видео инструкция:https://www.youtube.com/watch?v=FYwPdfxUvMs
+
+## Поддержать автора:
+* [Donation Alerts](https://www.donationalerts.com/r/black_triangle)
+
+* Bitcoin: 1AWBMoeV8UEybQi4QrQMmeFX1sXvRLDeCn
+
+* Ethereum: 0xB151c82A264eF0EA848c120444173658BFA18Cf9
+
+* DASH: XcNtFGW1ydGLudvTPWoBvPZZxG844EvksR
+
+* Zcash: t1hCJwasRozdkoaK9HLpngoEVPQhEZpxdFT
+
+* Monero: 41iNuQsc6GjZofH3XkKwNYVSXVsrjipfVjjNR3nbsL5XjJMFTfykW1T6CkYz1StdXH2t8dhnjUTT9FwEPpbsFVxjHuuYabQ
+
+* Litecoin: LNpw5QS5fvH1NW5AMp35zzMs1FYKuAUuPP
+
+* Bitcoin Cash: qrs29h849m9vj02ljd2utf6g7e7hxnntqvkhz8r8u3
+
+* Ethereum Classic: 0xc20a61Ec5765aC0059D6e85f069B440889EBfcC5
---
+++ start.py
@@ -0,0 +1,81 @@
+import sqlite3
+from telethon import TelegramClient
+import time
+import subprocess
+import sys
+import json
+
+db = sqlite3.connect('Account.db', timeout=30)
+cur = db.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS Account (
+    ID INTEGER PRIMARY KEY,
+    API_ID TEXT,
+    API_HASH TEXT,
+    NAME TEXT,
+    ID_SOB TEXT,
+    MY_ID TEXT
+)""")
+
+db.commit()
+
+
+def console_picture():
+    print("r0664n")
+    time.sleep(0.5)
+    print("power")
+
+    time.sleep(0.5)
+    print("with love")
+
+console_picture()
+print("Добро пожаловать в Telegram V2.0 antibroot! ")
+print("Важно: ")
+print("Вы и собеседник заранее должны договориться о пароле")
+print("Должны быть установлены все зависимости")
+print("В процессе конфигурирования нужно создать 2 клиента, поэтому логиниться придеться дважды")
+print("==============================")
+print("Нажми Enter чтобы запустить...")
+input()
+
+api_id = input("Введи свой Api_id: ")
+api_hash = input("Введи свой Api_hash: ")
+name = input("Введи ник собеседника? ")
+passworld = input("Введи общий пароль, с вашим собеседником: ")
+id_sob = "1"
+my_id = "1"
+
+with open("1.json") as f:
+	data = f.read()
+d = json.loads(data)
+d["password"] = passworld
+with open("1.json", 'w') as f:
+	f.write(json.dumps(d))
+       
+with open("2.json") as f2:
+	data2 = f2.read()
+d2 = json.loads(data2)
+d2["password"] = passworld
+with open("2.json", 'w') as f2:
+	f2.write(json.dumps(d2))
+
+cur.execute(f"SELECT API_ID FROM Account WHERE API_ID = '{api_id}'")
+if cur.fetchone() is None:
+    cur.execute("""INSERT INTO Account(API_ID, API_HASH, NAME, ID_SOB, MY_ID) VALUES (?,?,?,?,?);""", (api_id, api_hash, name, id_sob, my_id))
+    db.commit()
+    print("Зарегистрированно!")
+
+
+z = 1
+
+while(True):
+	session = "anon3" + str(z)
+	client = TelegramClient(session, api_id, api_hash)
+	client.start()
+	print("Аккаунт: " + str(z) + " Вход выполнен успешно!")
+	z = z+1
+	if z == 3:
+		print("Aккаунты активированы!")
+		break
+		
+
